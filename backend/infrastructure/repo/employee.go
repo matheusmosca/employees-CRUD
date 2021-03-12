32d42eee2e97cc4f567c2fa105497da1b4c3bb79
matheusmosca <matheusmoscag@gmail.com>
2021-03-11 21:56:32 -0300

    Add employee repository

--- backend/infrastructure/repo/employee.go
+++ backend/infrastructure/repo/employee.go
@@ -1,6 +1,9 @@
 package repo
 
 import (
+	"context"
+
+	"github.com/matheusmosca/nutcache/model"
 	"gorm.io/gorm"
 )
 
@@ -15,9 +18,45 @@ func NewEmployeeRepo(db *gorm.DB) EmployeeRepo {
 }
 
 type EmployeeRepo interface {
-	// CreateEmployee(context.Context, *model.Employee) error
+	Create(context.Context, *model.Employee) error
+	GetAll(context.Context) ([]*model.Employee, error)
+	GetByID(context.Context, int) (*model.Employee, error)
+	Update(context.Context, *model.Employee) error
+	Delete(context.Context, int) error
 }
 
-// func (r repo) CreateEmployee(ctx context.Context, employee *model.Employee) error {
-// 	r.db.Create()
-// }
+func (r employeeRepo) Create(ctx context.Context, employee *model.Employee) error {
+	err := r.db.Create(employee).Error
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r employeeRepo) GetAll(ctx context.Context) ([]*model.Employee, error) {
+	var employees []*model.Employee
+
+	err := r.db.Find(employees).Error
+	if err != nil {
+		return nil, err
+	}
+	return employees, nil
+}
+
+func (r employeeRepo) GetByID(ctx context.Context, ID int) (*model.Employee, error) {
+	var employee *model.Employee
+
+	err := r.db.First(&employee, ID).Error
+	if err != nil {
+		return nil, err
+	}
+	return employee, nil
+}
+
+func (r employeeRepo) Update(ctx context.Context, employee *model.Employee) error {
+	return r.db.Save(&employee).Error
+}
+
+func (r employeeRepo) Delete(ctx context.Context, ID int) error {
+	return r.db.Delete(&model.Employee{}, ID).Error
+}
